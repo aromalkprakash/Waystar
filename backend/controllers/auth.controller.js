8185d90bf19046b2f26757b4691dede560be1c5b
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-05-11 18:51:13 +0530

    auth.control and env

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+export const signup = async (req, res) => {
+    res.json({
+        data: "U hit the signup endpoint"
+    });
+}
+export const login= async (req, res) => {
+    res.json({
+        data: "U hit the login endpoint"
+}
+export const logout = async (req, res) => {
+    res.json({
+        data: "U hit the logout endpoint"
+    });
+}
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+import dotenv, { config } from "dotenv"
 
 const app = express();
+dotenv = config()
 
-app.get("/", (req, res) => {
-    res.send("Server is ready");
-})
+app.use("/api/auth", authRoutes); 
+
+console.log(process.env.MONGO_URI)
 
 app.listen(8000, () => {
-    console.log("server is running on port 8000")
-})
+    console.log("Server is running on port 8000");
+});
