c3602f90852e4244f15222871089b23b9ef44714
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-05-11 21:57:28 +0530

    user schema

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -1,14 +1,13 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
-    
-try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`mongodb connected ${conn.connection.host}`);
-} catch (error) {
-    console.log(`error to connect mongodb ${error.message}`);
-    process.exit(1);
-}
-}
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error connecting to MongoDB: ${error.message}`);
+        process.exit(1);
+    }
+};
 
 export default connectMongoDB;
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
-import dotenv, { config } from "dotenv"
+import dotenv from "dotenv"
 import connectMongoDB from "./db/connectMongoDB.js";
 
 const app = express();
 
 dotenv.config()
+console.log(process.env.MONGO_URI)
 
 app.use("/api/auth", authRoutes); 
 const PORT = process.env.PORT || 8000
 
-console.log(process.env.MONGO_URI)
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
