9e2e961a32ea90f0edc244a4d0411f5622fcc026
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-05-14 10:12:14 +0530

    suggested user

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { followUnfollowUser, updateUserProfile } from "../controllers/user.controller.js";
+import { followUnfollowUser, getSuggestedUsers, updateUserProfile } from "../controllers/user.controller.js";
 
 const router = express.Router();
 router.get("/profile/:username", protectRoute, updateUserProfile);
-// router.get("/suggested", protectRoute, getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
 // router.post("/update", protectRoute, updateUserProfile);
 
