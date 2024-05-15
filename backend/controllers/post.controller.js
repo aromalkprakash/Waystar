07e99bb771e729b1307fa6a95a9c8ca56db2860f
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-05-15 14:34:25 +0530

    get all posts

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,6 +1,7 @@
 import Post from "../models/post.model.js";
 import { v2 as cloudinary } from "cloudinary";
 import User from "../models/user.model.js";
+import Notification from "../models/notification.model.js";
 
 export const createPost = async (req, res) => {
     try {
@@ -83,3 +84,52 @@ export const commentOnPost = async (req, res) => {
         res.status(500).json({ error: "Internal server error" });
     }
 };
+
+export const likeUnlikePost = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const { id: postId } = req.params;
+
+        const post = await Post.findById(postId);
+
+        if (!post) {
+           return res.status(400).json({ error: "Post not found" });
+        }
+
+        const userLikedPost = post.likes.includes(userId);
+
+        if (userLikedPost) {
+            // unLikePost
+            await Post.updateOne({ _id: postId }, { $pull: { likes: userId } })
+            res.status(200).json({ message: "Post unLiked successfully" });
+        } else {
+            // like post
+            post.likes.push(userId);
+            await post.save();
+
+            const notifications = new Notification({
+                from: userId,
+                to: post.user,
+                type: "like"
+            })
+            await notifications.save();
+            res.status(200).json({message:"post liked successfully"})
+        }
+    } catch (error) {
+        console.log(error, "Error in the likeUnlikePost controller");
+        res.status(500).json({ error: "Internal server issue" });
+    }
+}
+
+export const getAllPosts = async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ createdAt: -1 });
+        if (posts.length === 0) {
+            return res.status(200).json([])
+        }
+        res.status(200).json(posts);
+    } catch (error) {
+        console.log(error, "Error in getAllPost controller");
+        res.status(500).json({error: "Internal server issue"})
+    }
+}
