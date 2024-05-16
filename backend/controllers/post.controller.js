4798f0a41bb4eb29f9ee334364aab0c8624e4938
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-05-16 10:13:42 +0530

    getlikedposts

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -100,7 +100,8 @@ export const likeUnlikePost = async (req, res) => {
 
         if (userLikedPost) {
             // unLikePost
-            await Post.updateOne({ _id: postId }, { $pull: { likes: userId } })
+            await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+            await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
             res.status(200).json({ message: "Post unLiked successfully" });
         } else {
             // like post
@@ -113,7 +114,8 @@ export const likeUnlikePost = async (req, res) => {
                 type: "like"
             })
             await notifications.save();
-            res.status(200).json({message:"post liked successfully"})
+            await User.updateOne({ _id: userId }, { $push: { likedPosts: postId } });
+            res.status(200).json({ message: "post liked successfully" });
         }
     } catch (error) {
         console.log(error, "Error in the likeUnlikePost controller");
@@ -123,7 +125,14 @@ export const likeUnlikePost = async (req, res) => {
 
 export const getAllPosts = async (req, res) => {
     try {
-        const posts = await Post.find().sort({ createdAt: -1 });
+        const posts = await Post.find().sort({ createdAt: -1 }).populate({
+            path: "user",
+            select: "-password"
+        })
+            .populate({
+                path: "comments.user",
+                select: "-password",
+        })
         if (posts.length === 0) {
             return res.status(200).json([])
         }
@@ -133,3 +142,28 @@ export const getAllPosts = async (req, res) => {
         res.status(500).json({error: "Internal server issue"})
     }
 }
+
+export const getLikedPosts = async (req, res) => {
+	const userId = req.params.id;
+
+	try {
+		const user = await User.findById(userId);
+		if (!user) return res.status(404).json({ error: "User not found" });
+
+		const likedPosts = await Post.find({ _id: { $in: user.likedPosts } })
+			.populate({
+				path: "user",
+				select: "-password",
+			})
+			.populate({
+				path: "comments.user",
+				select: "-password",
+			});
+
+		res.status(200).json(likedPosts);
+	} catch (error) {
+		console.log("Error in getLikedPosts controller: ", error);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
+
--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -29,7 +29,7 @@ const postSchema = new mongoose.Schema({
                 required: true
             },
         },
-    ],
+    ]
 },
     { timestamps: true });
 
