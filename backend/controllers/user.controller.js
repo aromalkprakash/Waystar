0ab30873067d7fff73e3b6cac5d1d271b970e681
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-05-16 11:54:25 +0530

    routes => route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -10,7 +10,7 @@ export const getUserProfile = async (req, res) => {
     const { username } = req.params;
 
     try {
-        const user = await User.findOne({ username }).select("password");
+        const user = await User.findOne({ username }).select("-password");
         if (!user){
             return res.status(401).json({ error: "User not found" });
         }
