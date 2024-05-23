4ba8217f828b74403542c7b891f7c4f23c13a437
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-05-23 20:36:48 +0530

    post created time

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -9,6 +9,7 @@ import {  useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast"
 
 import LoadingSpinner from "./LoadingSpinner"
+import { formatPostDate } from "../../../utils/date";
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
@@ -118,7 +119,7 @@ const Post = ({ post }) => {
 
 	const isMyPost = authUser._id === post.user._id;
 
-	const formattedDate = "1h";
+	const formattedDate = formatPostDate(post.createdAt);
 
 	const handleDeletePost = () => {
 		deletePost();
