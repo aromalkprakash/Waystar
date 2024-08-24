a49401006e9857ab2d0f64caa05dde61e22426f4
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-08-24 22:00:53 +0530

    Update LoginPage.jsx  xxxxx

--- frontend/src/Page/auth/login/LoginPage.jsx
+++ frontend/src/Page/auth/login/LoginPage.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-import XSvg from "../../../components/svgs/X";
 
 import { MdOutlineMail } from "react-icons/md";
 import { MdPassword } from "react-icons/md";
@@ -55,9 +54,7 @@ const LoginPage = () => {
 
 	return (
 		<div className='max-w-screen-xl mx-auto flex h-screen'>
-			{/* <div className='flex-1 hidden lg:flex items-center  justify-center'>
-				<XSvg className='lg:w-2/3 fill-white' />
-			</div> */}
+		
 			<div className='flex-1 flex flex-col justify-center items-center'>
 				<form className='flex gap-4 flex-col' onSubmit={handleSubmit}>
 					<XSvg className='w-24 lg:hidden fill-white' />
