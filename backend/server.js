a439fd475789faacc91bb20693f92c65a1d456d8
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-08-24 20:05:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000
 
 
 
-app.use(cors());
+app.use(cors({ origin: "https://waystar.onrender.com, credentials: true })); 
 app.use(express.json({limit:"5mb"}));  // parse req.body
 app.use(express.urlencoded({ extended: true }));
 
