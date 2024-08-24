a650a1c64f5167c9a25999d7d3ac2cbd746402ab
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-08-24 19:53:11 +0530

    Update LoginPage.jsx

--- frontend/src/Page/auth/login/LoginPage.jsx
+++ frontend/src/Page/auth/login/LoginPage.jsx
@@ -22,7 +22,7 @@ const LoginPage = () => {
 			
 		mutationFn: async ({ username, password }) => {
 			try {
-				const res = await fetch("https://waystar-backend.onrender.com/api/auth/login", {
+				const res = await fetch("api/auth/login", {
 					method: "POST",
 					headers: {
 						"Content-Type": "application/json",
